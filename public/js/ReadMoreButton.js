0ab1b27f10e49cc8e09d8c851dbffe8c0ce13418
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-18 07:06:11 -0500

    half box centered stuff set basic up for files

--- public/js/ReadMoreButton.js
+++ public/js/ReadMoreButton.js
@@ -1,14 +1,16 @@
+// ReadMoreButton.js
+
 $(document).ready(function() {
-    $("#toggle").click(function() {
-      var elem = $("#toggle").text();
-      if (elem == "Read More") {
-        //Stuff to do when btn is in the read more state
-        $("#toggle").text("Read Less");
-        $("#text").slideDown();
-      } else {
-        //Stuff to do when btn is in the read less state
-        $("#toggle").text("Read More");
-        $("#text").slideUp();
-      }
-    });
+  $("#toggle").click(function() {
+    var elem = $("#toggle").text();
+    if (elem == "Read More") {
+      // Stuff to do when the button is in the "Read More" state
+      $("#toggle").text("Read Less");
+      $("#text").slideDown();
+    } else {
+      // Stuff to do when the button is in the "Read Less" state
+      $("#toggle").text("Read More");
+      $("#text").slideUp();
+    }
   });
+});
