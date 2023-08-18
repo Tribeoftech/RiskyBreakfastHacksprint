273726a9cc5780be3c31b7ccede88c34d494ee3f
DiggDiggs <christopherdiggs1@gmail.com>
2023-08-17 23:30:17 -0500

    upload

--- serverNodejs/server.js
+++ serverNodejs/server.js
@@ -1,6 +1,24 @@
 const express = require('express');
+const { initializeApp } = require("firebase/app");
+const { getAnalytics } = require("firebase/analytics");
+
 const app = express();
 const PORT = 3000;
+
+// Your Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDI_zryNHSv777FF8gqIaf9p8yv21RQTLo",
+  authDomain: "risky-breakfast-f2f35.firebaseapp.com",
+  projectId: "risky-breakfast-f2f35",
+  storageBucket: "risky-breakfast-f2f35.appspot.com",
+  messagingSenderId: "542307173680",
+  appId: "1:542307173680:web:8b90eaf7dbd1e8ed3c74f5",
+  measurementId: "G-XXYWSDT91T"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const analytics = getAnalytics(firebaseApp);
 
 // Route handler for the root URL
 app.get('/', (req, res) => {
@@ -9,12 +27,12 @@ app.get('/', (req, res) => {
 
 // Route handler for a second route
 app.get('/food1', (req, res) => {
-  res.send('This is the About page.');
+  res.send('This is the Food 1 page.');
 });
 
 // Route handler for a third route
 app.get('/food2', (req, res) => {
-  res.send('This is the Contact page.');
+  res.send('This is the Food 2 page.');
 });
 
 // Start the server
